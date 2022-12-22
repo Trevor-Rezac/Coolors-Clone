6c459c83d9980c7906252d49243cfaedf1756d3b
Trevor Rezac <trev.rezac@gmail.com>
2022-12-22 14:30:24 -0800

    created random color generator using chroms.js  and set background and hex text to random colors

---
+++ app.js
@@ -0,0 +1,30 @@
+//SELECTORS and INITIALIZERS
+const generateBtn = document.querySelector(".generate");
+const colors = document.querySelectorAll(".color");
+const hexColors = document.querySelectorAll(".color h2");
+let initialColors;
+
+//EVENT LISTENERS
+
+//FUNCTIONS
+
+//Hex color generator
+function generateHexColor() {
+  const hexColor = chroma.random();
+  console.log("hexColor", hexColor);
+  return hexColor;
+}
+
+//Randomize palette
+function randomizePalette() {
+  colors.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomHexColor = generateHexColor();
+
+    div.style.backgroundColor = randomHexColor;
+    hexText.innerText = randomHexColor;
+  });
+}
+
+//Calling the function sets the initial palette
+randomizePalette();
